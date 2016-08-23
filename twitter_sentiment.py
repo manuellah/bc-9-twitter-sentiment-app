f05b6fc1aa380ac20ad30c4db9ceaf3229842767
manuellah <emmanumuthui@gmail.com>
2016-08-23 19:17:03 +0300

    added methods docstring for all my attributes and methods

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -1,6 +1,14 @@
 class TwitterSentiment(object):
+    '''
+    This class will fetch a particular tweeter user's frequent tweets, analyse the tweets and return the most used used words with their frequency
+    '''
+    #a list with stop words
     stop_words = []
+    
     def __init__(self, username):
+        '''
+        initialises the username whose data is to be fetched(tweets)
+        '''
         self.username = username
         
     def fetch_data(self):
@@ -10,4 +18,8 @@ class TwitterSentiment(object):
         pass
     
     def analyse_data(self):
+        '''
+        This method takes the json file created in fetch data method and analyses the most frequent tweets words for that  user eliminating the stop words.
+        Return a with dictictionary where word is a key and the frequency is the value) 
+        '''
         pass
