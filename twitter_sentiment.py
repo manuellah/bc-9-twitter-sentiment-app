6bd1c6ac64aa71f49a7849ea23c53f44278f9e7a
manuellah <emmanumuthui@gmail.com>
2016-08-23 19:10:15 +0300

    added methods docstring

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -4,6 +4,9 @@ class TwitterSentiment(object):
         self.username = username
         
     def fetch_data(self):
+        '''
+        This method fetchs data from twitter for a specific user and stores it in a json file
+        '''
         pass
     
     def analyse_data(self):
