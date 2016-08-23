c4b12716ce010ec02b3f9edb15ccfab4da8b880e
manuellah <emmanumuthui@gmail.com>
2016-08-23 19:02:36 +0300

    added my main class twitter_sentiment.py and the skeloton of my class

---
+++ twitter_sentiment.py
@@ -0,0 +1,10 @@
+class TwitterSentiment(object):
+    stop_words = []
+    def __init__(self, username):
+        self.username = username
+        
+    def fetch_data(self):
+        pass
+    
+    def analyse_data(self):
+        pass
