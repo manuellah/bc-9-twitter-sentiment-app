b79f518df51740e2c710b4661683614c664d1b04
manuellah <emmanumuthui@gmail.com>
2016-08-25 10:54:57 +0300

    tweak the fetch_method to be able to query a specific number of tweets

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -1,20 +1,10 @@
-import tweepy
+import tweepy, progressbar
+from config import *
 
 class TwitterSentiment(object):
     '''
     This class will fetch a particular tweeter user's frequent tweets, analyse the tweets and return the most used used words with their frequency
     '''
-    #####  CLASS VARIABLES    ######
-    
-    #a list with stop words
-    stop_words = ["a", "about", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
-
-    
-    #Variables that contains the user credentials to access Twitter API 
-    API_KEYS='EzfVwFMj3IzAUXMzvXLX9gHaF'
-    API_SECRET= "Y1Gnr2oklcYGFbTryr3yDiiXgIWKAmPUVyouyr4NbRg8wmsjMT"
-    ACCESS_TOKEN = "588855017-vZ5eQksRsgei2Jc2Wfev22DY2yWdk748ds7EiHFb"
-    ACCESS_TOKEN_SECRET = "dJvDBNYHxhDO67I1B61bsd7P7S1j1PCkee4LI7fLjVW2b"
     
     # OAuth Authentication. This is the authidication for using the twitter api
     auth = tweepy.OAuthHandler(API_KEYS, API_SECRET)
@@ -25,38 +15,50 @@ class TwitterSentiment(object):
     api = tweepy.API(auth)
     
     
-    def __init__(self, username):
+    def __init__(self, username, number=3200):
         '''
         initialises the username whose data is to be fetched(tweets)
         '''
         
         self.username = username
+        self.no_tweets = number
         #print(TwitterSentiment.api.me().name)
         
 
-    def fetch_data(self):
+    def fetch_data(self ):
         '''
         This method fetchs data from twitter for a specific user and stores it in a json file
         '''
         my_dict = dict()
         alltweets = list()
-        new_tweets = TwitterSentiment.api.user_timeline(self.username, count=200, max_id= 0)
-        new_tweets = TwitterSentiment.api.user_timeline( self.username, count=200)
-        alltweets.extend(new_tweets)
-        if not alltweets:
-            my_dict[0] = "No tweet so far","00/00/00"
-            return my_dict
-        
-        oldest = alltweets[-1].id - 1
-	
-        #keep grabbing tweets until there are no tweets left to grab
-        while new_tweets:
-            new_tweets = TwitterSentiment.api.user_timeline(self.username, count=200, max_id=oldest)
+        if self.no_tweets <= 200:
+            new_tweets = TwitterSentiment.api.user_timeline(self.username, count = self.no_tweets)
             alltweets.extend(new_tweets)
+            if not alltweets:
+                my_dict[0] = "No tweet so far","00/00/00"
+                return my_dict
+
+        else:    
+            new_tweets = TwitterSentiment.api.user_timeline(self.username, count=200)
+            alltweets.extend(new_tweets)
+            self.no_tweets -= 200
             oldest = alltweets[-1].id - 1
+
+            #keep grabbing tweets until there are no tweets left to grab
+            while new_tweets:
+                if self.no_tweets <= 200:
+                    new_tweets = TwitterSentiment.api.user_timeline(self.username, count = self.no_tweets)
+                    alltweets.extend(new_tweets)
+                    
+                else:
+                    new_tweets = TwitterSentiment.api.user_timeline(self.username, count=200, max_id=oldest)
+                    alltweets.extend(new_tweets)
+                    oldest = alltweets[-1].id - 1
+                    self.no_tweets -= 200
+                
         #save the tweet text in a dictionary mapped against the specific tweet id    
         for tweet in alltweets:
-            my_dict[tweet.id] = (tweet.text,tweet.created_at)
+                my_dict[tweet.id] = (tweet.text, tweet.created_at)
         return my_dict
     
     def display_tweets(self):
@@ -77,7 +79,7 @@ class TwitterSentiment(object):
         for key in tweet_dict:
             tweet_words = str(tweet_dict[key][0]).lower().split()
             for word in tweet_words:
-                if word in TwitterSentiment.stop_words:
+                if word in stop_words:
                     continue
                 elif word not in list(freq_dict):
                     freq_dict[word] = 1
@@ -91,8 +93,9 @@ class TwitterSentiment(object):
             print(' {} {}'.format(word.ljust(30), str(freq).ljust(30)))
     
         
-user=  TwitterSentiment("emmanuelmuthui")
-#user.display_tweets()
+user=  TwitterSentiment("emmanuelmuthui",30)
+user.fetch_data()
+user.display_tweets()
 print(user.analyse_data())
 
 #user=  TwitterSentiment("Emmanuel")
